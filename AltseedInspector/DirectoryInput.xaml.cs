a092d4b3ab99bdf4bd3dccff67d68b61b5884e92
GON <hutabasuzukiryu@live.jp>
2019-08-29 14:54:07 +0900

    add: Inspector Controls

---
+++ AltseedInspector/DirectoryInput.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AltseedInspector
+{
+    /// <summary>
+    /// DirectoryInput.xaml の相互作用ロジック
+    /// </summary>
+    public partial class DirectoryInput : UserControl
+    {
+        public DirectoryInput()
+        {
+            InitializeComponent();
+        }
+    }
+}
