d05f00ed1084c43703e09c1779b141d3fc4cf513
GON <hutabasuzukiryu@live.jp>
2019-11-20 12:45:38 +0900

    clean

--- InspectorModel/DirectoryInputAttribute.cs
+++ InspectorModel/DirectoryInputAttribute.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace InspectorModel
 {
@@ -12,7 +8,7 @@ namespace InspectorModel
     [AttributeUsage(AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
     public sealed class DirectoryInputAttribute : BaseAttribute
     {
-        public DirectoryInputAttribute(string itemName,　string rootPathBinding = null)
+        public DirectoryInputAttribute(string itemName, string rootPathBinding = null)
         {
             ItemName = itemName;
             RootPathBinding = rootPathBinding;
--- InspectorTest/TestModel.cs
+++ InspectorTest/TestModel.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using InspectorModel;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using InspectorModel;
 
 namespace InspectorTest
 {
